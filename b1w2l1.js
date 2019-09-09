54d8077ebbd5d028f11b88bf8233f4404030b966
jelledekok <99061618@mydavinci.nl>
2019-09-09 14:24:03 +0200

    de codes er in gezet

--- b1w2l1.js
+++ b1w2l1.js
@@ -1,1 +1,10 @@
-// begin hier je JavaScript commandos
+/*
+jelle de kok
+ICT BOL4 Cohort
+*/
+alert('je moet een paar vragen invullen')//vraagt of je een paar vragen wilt invullen
+document.write(prompt('vul hier je voornaam in'))//laat je voornaam zien als je die invult
+document.write(prompt('vul hier je achternaam in'))//en hier je achternaam
+document.write(prompt('vul hier je leeftijd in'))//en hier je leeftijd
+
+
