fc5862b028c969b7c7d929ee951b744bf95f163a
jelledekok <99061618@mydavinci.nl>
2019-09-11 10:26:15 +0200

    bijna klaar met calculator

---
+++ pizzacalculator.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+const welkom = 'welkom bij de pizzacalculator';
+alert(welkom);
+var kleinepizza = prompt('vul in hoeveel small pizzas u wilt ');
+var mediumpizza = prompt('vul in hoeveel medium pizzas u wilt');
+var largepizza = prompt('vul in hoeveel large pizzas u wilt');
+var kosten = ((4*kleinepizza)+(6*mediumpizza)+(7.50*largepizza));
+
+
+document.write('Small ' + kleinepizza + ' x 4.00 = '+(kleinepizza)+');
+
+
+//12+6×10/5-10 = '+(12+6*10/5-10))
+
+document.write(kosten);
