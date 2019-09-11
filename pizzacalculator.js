ad7a5855a23649cb70c4d059209300082a0d3ea5
jelledekok <99061618@mydavinci.nl>
2019-09-11 13:21:12 +0200

    bijna klaar

--- pizzacalculator.js
+++ pizzacalculator.js
@@ -1,20 +1,21 @@
-
-
-
-
+/*
+jelle de kok
+ICT BOL4 Cohort 
+pizzacalculator
+*/
 
 
 const welkom = 'welkom bij de pizzacalculator';
 alert(welkom);
-var kleinepizza = prompt('vul in hoeveel small pizzas u wilt ');
+var kleinepizza = parseInt(prompt('vul in hoeveel small pizzas u wilt '));
 var mediumpizza = prompt('vul in hoeveel medium pizzas u wilt');
 var largepizza = prompt('vul in hoeveel large pizzas u wilt');
 var kosten = ((4*kleinepizza)+(6*mediumpizza)+(7.50*largepizza));
 
 
-document.write('Small ' + kleinepizza + ' x 4.00 = '+(kleinepizza)+');
+document.write('Small ' + kleinepizza + ' x 4.00 = '+ (kleinepizza*4));
 
 
-//12+6×10/5-10 = '+(12+6*10/5-10))
 
-document.write(kosten);
+
+document.write('<br/>totaal ' + kosten);
